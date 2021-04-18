e573e50a39f2ba854b928ae4e713ad5934dcb37b
Michal G <miki.glodek@wp.pl>
2021-04-18 19:27:34 +0200

    enable cors

--- src/main/java/com/example/learningapplicationwithshop/security/SecurityConfig.java
+++ src/main/java/com/example/learningapplicationwithshop/security/SecurityConfig.java
@@ -17,9 +17,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @EnableWebSecurity
@@ -61,7 +63,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http = http.cors().and().csrf().disable();
+        http = http.csrf().disable();
 
         http = http
                 .sessionManagement()
