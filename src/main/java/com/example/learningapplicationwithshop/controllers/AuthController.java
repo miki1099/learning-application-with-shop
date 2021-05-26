67885c5991040fa8d1ba24f441e6f08cde049fda
Michal G <miki.glodek@wp.pl>
2021-05-26 20:59:31 +0200

    Bugfix refreshing token endpoint

--- src/main/java/com/example/learningapplicationwithshop/controllers/AuthController.java
+++ src/main/java/com/example/learningapplicationwithshop/controllers/AuthController.java
@@ -4,7 +4,9 @@ import com.example.learningapplicationwithshop.model.User;
 import com.example.learningapplicationwithshop.model.dto.UserLoginDto;
 import com.example.learningapplicationwithshop.model.dto.UserLoginReturnDto;
 import com.example.learningapplicationwithshop.security.JwtTokenUtil;
+import com.example.learningapplicationwithshop.services.UserService;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,6 +25,8 @@ public class AuthController {
 
     private final AuthenticationManager authenticationManager;
     private final JwtTokenUtil jwtTokenUtil;
+    private final UserService userService;
+    private final ModelMapper modelMapper;
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
     public ResponseEntity<UserLoginReturnDto> login(@RequestBody UserLoginDto user) {
@@ -51,7 +55,7 @@ public class AuthController {
 
     @PostMapping(value = "/refreshToken")
     public ResponseEntity<UserLoginReturnDto> refreshToken(Principal principal) {
-        User userAuth = (User) principal;
+        User userAuth = modelMapper.map(userService.findByLogin(principal.getName()), User.class);
         UserLoginReturnDto userReturn = new UserLoginReturnDto();
         userReturn.setLogin(userAuth.getLogin());
         userReturn.setToken(jwtTokenUtil.generateAccessToken(userAuth));
